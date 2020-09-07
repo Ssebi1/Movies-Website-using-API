66a61f72c54b687f0a456fe2a2ebeebe460828e5
Ssebi <sebidancau@yahoo.com>
2020-09-07 15:06:53 +0300

    commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT | 8080;
+
+app.listen(port);
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => res.render('index.ejs'));
