481531c6d8c1c8100ceb285b23934b7fa8d407dd
Ssebi <sebidancau@yahoo.com>
2020-09-10 08:30:31 +0300

    final improvments on mobile

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ dotenv.config({ path: './.env' });
 app.listen(port);
 
 app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
